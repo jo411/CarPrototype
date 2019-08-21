9f30e6a47e1b956c814035af9cb557756b8e29a7
Josh Nelson <josh.nelson1@yahoo.com>
2019-08-20 23:11:58 -0600

    Initial commit

---
+++ CarProto/Game1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using GeonBit;
+using GeonBit.ECS;
+using GeonBit.ECS.Components;
+using GeonBit.ECS.Components.Graphics;
+using GeonBit.ECS.Components.Misc;
+using GeonBit.ECS.Components.Particles;
+using GeonBit.ECS.Components.Physics;
+using GeonBit.ECS.Components.Sound;
+
+namespace CarProto
+{
+    /// <summary>
+    /// Your main game class!
+    /// </summary>
+    internal class Game1 : GeonBitGame
+    {
+        /// <summary>
+        /// Initialize your GeonBitGame properties here.
+        /// </summary>
+        public Game1()
+        {
+            InitParams.UiTheme = "hd";
+            InitParams.DebugMode = true;
+            InitParams.EnableVsync = true;
+            InitParams.Title = "New GeonBit Project";
+            InitParams.FullScreen = false;
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        override public void Update(GameTime gameTime)
+        {
+            /// exit application on escape
+            if (Managers.GameInput.IsKeyDown(GeonBit.Input.GameKeys.Escape))
+            {
+                Exit();
+            }
+
+            /// TBD add any custom Update functionality here.
+        }
+
+        /// <summary>
+        /// Initialize to implement per main type.
+        /// </summary>
+        override public void Initialize()
+        {
+            /// TBD create your scene, components and init resources here.
+            /// The code below contains a simple example of how to use UI, camera, and basic entity renderer.
+
+            /// Example 1: create UI text
+            ActiveScene.UserInterface.AddEntity(new GeonBit.UI.Entities.Paragraph("Welcome to GeonBit! Here's a sphere:"));
+
+            /// Example 2: create camera and add to scene
+            GameObject cameraObject = new GameObject("camera");
+            cameraObject.AddComponent(new Camera());
+            cameraObject.SceneNode.PositionZ = 5;
+            cameraObject.Parent = ActiveScene.Root;
+
+            /// Example 3: add 3d shape to scene
+            GameObject shapeObject = new GameObject("shape");
+            shapeObject.AddComponent(new ShapeRenderer(ShapeMeshes.SphereSmooth));
+            shapeObject.Parent = ActiveScene.Root;
+
+            // add diagnostic data paragraph to scene
+            var diagnosticData = new GeonBit.UI.Entities.Paragraph("", GeonBit.UI.Entities.Anchor.BottomLeft, offset: Vector2.One * 10f, scale: 0.7f);
+            diagnosticData.BeforeDraw = (GeonBit.UI.Entities.Entity entity) =>
+            {
+                diagnosticData.Text = Managers.Diagnostic.GetReportString();
+            };
+            ActiveScene.UserInterface.AddEntity(diagnosticData);
+        }
+
+        /// <summary>
+        /// Draw function to implement per main type.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        override public void Draw(GameTime gameTime)
+        {
+            /// TBD add any custom drawing functionality here.
+            /// Note: since GeonBit handle its own drawing internally, usually you don't need to do anything here.
+        }
+    }
+}
