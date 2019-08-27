3d73907d66293b3e24f40321009069740f8bf5a6
Josh Nelson <josh.nelson1@yahoo.com>
2019-08-27 11:50:16 -0600

    Added Models files. Created a rotating object component. Added the builder UI.

---
+++ CarProto/CustomComponents/RotatingObject.cs
@@ -0,0 +1,34 @@
+﻿using GeonBit.ECS.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProto.CustomComponents
+{
+    class RotatingObject : BaseComponent
+    {
+        float speedX;
+        float speedY;
+        float speedZ;
+        public RotatingObject(float speedX, float speedY, float speedZ)
+        {
+            this.speedX = speedX;
+            this.speedY = speedY;
+            this.speedZ = speedZ;
+        }
+
+        protected override void OnUpdate()
+        {
+            _GameObject.SceneNode.RotationX += speedX * Managers.TimeManager.TimeFactor;
+            _GameObject.SceneNode.RotationY += speedY * Managers.TimeManager.TimeFactor;
+            _GameObject.SceneNode.RotationZ += speedZ * Managers.TimeManager.TimeFactor;
+        }
+
+        public override BaseComponent Clone()
+        {
+            return new RotatingObject(speedX, speedY,speedZ);
+        }
+    }
+}
